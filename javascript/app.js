a47fe7f8c8b79cb6fc360bf74dd8a5a27fbd4681
Michael Carr <mcarr2468@gmail.com>
2021-04-15 18:13:25 -0500

    V1.0

---
+++ javascript/app.js
@@ -0,0 +1,48 @@
+/*Test data(want to use an API for the actual data)
+----------
+Pizza
+
+Pizza is a savory dish of Italian origin consisting of a usually round, flattened base of leavened wheat-based dough topped with tomatoes, cheese, and often various other ingredients (such as anchovies, mushrooms, onions, olives, pineapple, meat, etc.), which is then baked at a high temperature, traditionally in a wood-fired oven. A small pizza is sometimes called a pizzetta. A person who makes pizza is known as a pizzaiolo.
+----------
+Cake
+
+Cake is a form of sweet food made from flour, sugar, and other ingredients, that is usually baked. In their oldest forms, cakes were modifications of bread, but cakes now cover a wide range of preparations that can be simple or elaborate, and that share features with other desserts such as pastries, meringues, custards, and pies.
+----------
+Sushi
+
+Sushi (すし, 寿司, 鮨, pronounced [sɯɕiꜜ] or [sɯꜜɕi]) is a traditional Japanese dish of prepared vinegared rice (鮨飯, sushi-meshi), usually with some sugar and salt, accompanying a variety of ingredients (ネタ, neta), such as seafood, often raw, and vegetables. Styles of sushi and its presentation vary widely, but the one key ingredient is "sushi rice", also referred to as shari (しゃり), or sumeshi (酢飯).
+----------
+----------
+----------
+the description are from
+the first paragraph of the
+wikipedia article of the given food.
+*/
+
+
+/*TODO
+1. create the food data(that your going to swipe on)
+- set up the food constructor
+- create instances of that constructor(eg: pizza, sushi, ect...)
+- put it into the DOM
+
+2. create a working dislike button
+- just move to the next food / slide
+- remove it from the list of food your being shown
+
+3. create a working like button
+- store the food in a data structure,
+  so you can use that food data later
+  (to find location to eat that food at)
+
+4. create the results tab / get it working
+- create the HTML / CSS
+- show the all the(saved) food that they liked,
+and location where they can order that food at
+AND a recipe(s) that they can use to make that food themselves.
+    - recipe can either be from youtube or a recipe api
+
+Additional notes / features
+    - when you have swiped through all the food,
+    show a no more food left to show screen
+*/
