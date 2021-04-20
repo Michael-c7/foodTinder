58fba476b6e695d5583f5e8216dc15965c6ce848
Michael Carr <mcarr2468@gmail.com>
2021-04-20 17:46:10 -0500

    V1.6

--- javascript/app.js
+++ javascript/app.js
@@ -24,6 +24,12 @@ wikipedia article of the given food.
 4. create the results tab / get it working
 - create the HTML / CSS [X]
 
+- get the tabs working
+    - when you click on the swipe tab shows the food profile
+    - when you click on the results tab  shows what food you liked
+
+- show all the (saved) food that they liked
+
 - Click on the recipe btn(book icon) & get linked to
 https://www.allrecipes.com/search/results/?search=food
 (of the current food)
@@ -31,11 +37,6 @@ https://www.allrecipes.com/search/results/?search=food
 - Click on the direction btn(book icon) & get linked to
 https://www.google.com/maps/search/food/@Xlocation,yLocation
 (of the current food)
-
-- show the all the(saved) food that they liked,
-and location where they can order that food at
-AND a recipe(s) that they can use to make that food themselves.
-    - recipe can either be from a recipe api
 
 Additional notes / features
     - when you have swiped through all the food,
@@ -80,8 +81,11 @@ let profilesEl = document.querySelector(".profile-section__profiles")
 let profileEl = document.querySelector(".profile__info")
 let likeBtnEl = document.querySelector(".button-choice-like")
 let dislikeBtnEl = document.querySelector(".button-choice-dislike")
-let likedFoodProfiles = []
-
+let profileChoiceBtnContainer = document.querySelector(".button-choice-section")
+let appMenuSwipeBtnEl = document.querySelector(".app__menu__swipe")
+let appMenuResultEl = document.querySelector(".app__menu__result")
+let savedFoodEl = document.querySelector(".profile-section__saved-food")
+let likedFoodProfiles = ["chicken", "noodles"]
 
 
 
@@ -264,3 +268,99 @@ likeBtnEl.addEventListener("click", likeButton);
 
 
 /*results tab*/
+// SWIPE
+//appMenuSwipeBtnEl
+
+    // profilesEl
+    // profileChoiceBtnContainer
+// RESULT
+// appMenuResultEl
+    // savedFoodEl
+
+
+
+appMenuSwipeBtnEl.addEventListener("click", function() {
+    //
+    appMenuResultEl.classList.remove("app__menu__choice--active")
+    appMenuSwipeBtnEl.classList.add("app__menu__choice--active")
+    //
+    profilesEl.classList.remove("deactive")
+    profileChoiceBtnContainer.classList.remove("deactive")
+    savedFoodEl.classList.add("deactive")
+});
+
+appMenuResultEl.addEventListener("click", function() {
+    //
+    appMenuSwipeBtnEl.classList.remove("app__menu__choice--active")
+    appMenuResultEl.classList.add("app__menu__choice--active")
+    //
+    savedFoodEl.classList.remove("deactive")
+    profilesEl.classList.add("deactive")
+    profileChoiceBtnContainer.classList.add("deactive")
+})
+
+
+
+
+const renderResults = _ => {
+    /*get the food data into the DOM*/
+    likedFoodProfiles.forEach(likedFood => {
+
+        // ul === profilesEl
+        /*document fragment*/
+            let fragment = document.createDocumentFragment();
+            // items
+            let li = document.createElement("li");
+            let p = document.createElement("p");
+            let div = document.createElement("div");
+            let buttonRecipe  = document.createElement("button");
+                let aRecipe = document.createElement("a");
+                let imgRecipe = document.createElement("img");
+            let buttonDirections  = document.createElement("button");
+                let aDirections = document.createElement("a");
+                let imgDirections = document.createElement("img");
+            let buttonDelete  = document.createElement("button");
+                let imgDelete = document.createElement("img");
+            // classes
+            li.classList.add("saved-food__item")
+            p.classList.add("saved-food__item__food")
+            div.classList.add("save-food__item__btns")
+            buttonRecipe.classList.add("save-food-item-btn", "saved-food__item__recipe-btn")
+            aRecipe.classList.add("save-food-item-btn__link")
+            imgRecipe.classList.add("icon", "icon--save-food")
+            buttonDirections.classList.add("save-food-item-btn", "saved-food__item__directions-btn")
+            imgDirections.classList.add("icon", "icon--save-food")
+            buttonDelete.classList.add("save-food-item-btn", "saved-food__item__delete-btn")
+            imgDelete.classList.add("icon", "icon--save-food", "delete-btn-icon")
+            // attributes
+            aRecipe.href = `https://www.allrecipes.com/search/results/?search=${likedFood}`;
+            aRecipe.target = "_blank";
+            imgRecipe.src = "./images/icons/book-reader-solid-white.svg";
+            imgRecipe.alt = "recipe icon";
+            aDirections.href = `https://www.google.com/maps/search/${likedFood}`;
+            aDirections.target = "_blank";
+            imgDirections.src = "./images/icons/compass-solid-white.svg";
+            imgDirections.alt = "directions icon";
+            imgDelete.src = "./images/icons/times-solid-white.svg";
+            imgDelete.alt = "delete icon";
+            //  text / inner values
+            p.innerHTML = likedFood;
+            // appending everything
+            fragment.appendChild(li);
+            li.appendChild(p);
+            li.appendChild(div);
+            div.appendChild(buttonRecipe);
+            buttonRecipe.appendChild(aRecipe);
+            aRecipe.appendChild(imgRecipe);
+            div.appendChild(buttonDirections);
+            buttonDirections.appendChild(aDirections);
+            aDirections.appendChild(imgDirections);
+            div.appendChild(buttonDelete);
+            buttonDelete.appendChild(imgDelete);
+            // append the fragment to the DOM
+            savedFoodEl.appendChild(fragment);
+    });
+}
+
+
+renderResults()
